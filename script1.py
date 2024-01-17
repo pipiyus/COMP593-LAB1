43b6c125b346eb071138e7c0791edd106ee208da
pipiyus <156553508+pipiyus@users.noreply.github.com>
2024-01-17 18:22:31 -0500

    Modified both script files.
    -Deleted print from script1.py
    -added comment to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # print lines from Zen of Python
 print("simple is better than complex.")
-print("now is better than never.")
--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+# print lines from Zen of Python
 print("unless explicity silenced.")
 print("flat is better than nested.")
